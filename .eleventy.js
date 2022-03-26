6360268f6473306998384f7711342a2303f555e6
baskie <neil@bascombe.net>
2022-03-26 22:56:30 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/images/logos");
   eleventyConfig.addPassthroughCopy("./src/scripts");
 
     return {
