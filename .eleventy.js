8f985436dcffa7b43a37c114e9c6e498fcd12904
baskie <neil@bascombe.net>
2023-03-05 08:35:26 +0000

    removed viewport for results zooming

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/pdfs");
   eleventyConfig.addPassthroughCopy("./src/docs");
   eleventyConfig.addPassthroughCopy("./src/email-form.php");
-  eleventyConfig.addPassthroughCopy("./src/draw");
+
     return {
       dir: {
         input: "src",
